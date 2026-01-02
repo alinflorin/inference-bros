f574b4fd7e9217b5279fd479ed3ab1bca2c14cb7
Alin-Florin Ciu <alinflorinciu@icloud.com>
2026-01-02 22:56:37 +0200

    -

--- variables.tf
+++ variables.tf
@@ -3,11 +3,27 @@ variable "ssh_private_key" {
   sensitive   = true
 }
 
+variable "ssh_public_key" {
+  type        = string
+  sensitive   = true
+}
+
+variable "root_ca_key" {
+  type        = string
+  sensitive   = true
+}
+
+variable "root_ca_crt" {
+  type        = string
+  sensitive   = true
+}
+
 variable "servers" {
   type = list(object({
     ip     = string
     port   = number
     user   = string
     master = bool
+    iface = string
   }))
 }
