ce8db30259260961bcfd84b7586469003e53a9f6
Alin-Florin Ciu <alinflorinciu@icloud.com>
2026-01-03 23:03:15 +0200

    asd

---
+++ ingress-nginx.tf
@@ -0,0 +1,23 @@
+resource "helm_release" "ingress_nginx" {
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  namespace  = "ingress-nginx"
+  create_namespace = true
+  version = "4.14.1"
+  atomic          = true
+  wait            = true
+
+  values = [
+    <<-EOT
+      controller:
+        metrics:
+          enabled: true
+          serviceMonitor:
+            enabled: true
+    EOT
+
+  ]
+
+  depends_on = [helm_release.metallb_config]
+}
