fb781f6a61f8390ee071d3e3db31948bdf37f049
Alin-Florin Ciu <alinflorinciu@icloud.com>
2026-01-03 23:17:17 +0200

    sdf

---
+++ system-upgrade-controller.tf
@@ -0,0 +1,19 @@
+resource "helm_release" "system_upgrade_controller" {
+  name       = "system-upgrade-controller"
+  repository = "https://nimbolus.github.io/helm-charts"
+  chart      = "system-upgrade-controller"
+  namespace  = "kube-system"
+  create_namespace = true
+  version = "0.7.0"
+  atomic          = true
+  wait            = true
+
+  values = [
+    <<-EOT
+
+    EOT
+
+  ]
+
+  depends_on = [null_resource.k3s_installed]
+}
