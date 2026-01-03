9a205877086673c411bf48b99074fa58aa1c23e8
Alin-Florin Ciu <alinflorinciu@icloud.com>
2026-01-03 10:51:11 +0200

    -

--- variables.tf
+++ variables.tf
@@ -1,33 +1,33 @@
 variable "ssh_private_key" {
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "ssh_public_key" {
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "letsencrypt_email" {
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "k3s_vip" {
-  type        = string
+  type = string
 }
 
 variable "domain" {
-  type        = string
+  type = string
 }
 
 variable "servers" {
   type = list(object({
-    ip     = string
+    ip       = string
     hostname = string
-    port   = number
-    user   = string
-    master = bool
-    iface = string
+    port     = number
+    user     = string
+    master   = bool
+    iface    = string
   }))
 }
