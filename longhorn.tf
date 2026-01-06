885eefc12e8caf39335a4601c61f5365809d467b
Alin-Florin Ciu <alinflorinciu@icloud.com>
2026-01-07 00:26:56 +0200

    asd

--- longhorn.tf
+++ longhorn.tf
@@ -22,7 +22,7 @@ resource "helm_release" "longhorn" {
         annotations:
           nginx.ingress.kubernetes.io/ssl-redirect: 'true'
           cert-manager.io/cluster-issuer: ${var.location == "local" ? "root-ca-issuer" : "letsencrypt"}
-          nginx.ingress.kubernetes.io/auth-url: "https://oauth2-proxy.${var.domain}/oauth2/auth"
+          nginx.ingress.kubernetes.io/auth-url: "https://oauth2-proxy.oauth2-proxy.svc.cluster.local/oauth2/auth"
           nginx.ingress.kubernetes.io/auth-signin: "https://oauth2-proxy.${var.domain}/oauth2/start?rd=$scheme://$host$request_uri"
           nginx.ingress.kubernetes.io/server-snippet: |
             resolver kube-dns.kube-system.svc.cluster.local valid=10s;
