a23749eb2c3765d6c165f66c4500b70ac6200768
Alin-Florin Ciu <alinflorinciu@icloud.com>
2026-01-07 22:45:58 +0200

    asd

---
+++ kserve.tf
@@ -0,0 +1,72 @@
+resource "helm_release" "kserve_crd" {
+  name             = "kserve-crd"
+  repository       = "oci://ghcr.io/kserve/charts/kserve-crd"
+  chart            = "kserve-crd"
+  namespace        = "kserve"
+  create_namespace = true
+  version          = "v0.15.0"
+  atomic           = true
+  wait             = true
+
+  values = [
+    <<-EOT
+
+    EOT
+
+  ]
+
+  depends_on = [helm_release.grafana]
+}
+
+resource "helm_release" "kserve" {
+  name             = "kserve"
+  repository       = "oci://ghcr.io/kserve/charts/kserve"
+  chart            = "kserve"
+  namespace        = "kserve"
+  create_namespace = true
+  version          = "v0.15.0"
+  atomic           = true
+  wait             = true
+
+  values = [
+    <<-EOT
+kserve:
+  controller:
+    deploymentMode: Standard
+    gateway:
+      domain: "kserve.${var.domain}"
+      disableIstioVirtualHost: true
+      ingressGateway:
+        className: "nginx"
+    resources:
+      limits:
+        cpu: ''
+        memory: ''
+      requests:
+        cpu: ''
+        memory: ''
+    rbacProxy:
+      resources:
+        limits:
+          cpu: ''
+          memory: ''
+        requests:
+          cpu: '
+          memory: ''
+  storage:
+    resources:
+      requests:
+        memory: ''
+        cpu: ''
+      limits:
+        memory: ''
+        cpu: ''
+  metricsaggregator:
+    enableMetricAggregation: "true"
+    enablePrometheusScraping: "true"
+    EOT
+
+  ]
+
+  depends_on = [helm_release.kserve_crd]
+}
