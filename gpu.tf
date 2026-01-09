6f4aa9516a9351db372d15b9bdc002d449f5817b
Alin-Florin Ciu <alinflorinciu@icloud.com>
2026-01-09 17:36:45 +0200

    -

--- gpu.tf
+++ gpu.tf
@@ -18,7 +18,7 @@ resource "helm_release" "nvidia_gpu_operator" {
 
   count = var.kubeai_compute_processor == "nvidia" ? 1 : 0
 
-  depends_on = [helm_release.cert_manager_self_signed_issuer
+  depends_on = [helm_release.cert_manager_self_signed_issuer]
 }
 
 resource "helm_release" "amd_gpu_operator" {
@@ -40,5 +40,5 @@ resource "helm_release" "amd_gpu_operator" {
 
   count = var.kubeai_compute_processor == "amd" ? 1 : 0
 
-  depends_on = [helm_release.cert_manager_self_signed_issuer
+  depends_on = [helm_release.cert_manager_self_signed_issuer]
 }
