a7a2c72ab90afa3aab5506fe700cd1a364b38d30
Alin Florin <alinflorinciu@icloud.com>
2026-01-18 02:54:04 +0200

    asd

---
+++ kmm.tf
@@ -0,0 +1,33 @@
+resource "helm_release" "kmm" {
+  name             = "kmm"
+  repository       = "oci://ghcr.io/alinflorin/charts"
+  chart            = "kmm"
+  namespace        = "kubeai"
+  create_namespace = false
+  version          = "1.0.9"
+  atomic           = true
+  wait             = true
+
+  values = [
+    <<-EOT
+      ingress:
+        enabled: true
+        className: "nginx"
+        annotations:
+          nginx.ingress.kubernetes.io/ssl-redirect: 'true'
+          cert-manager.io/cluster-issuer: ${var.location == "local" ? "root-ca-issuer" : "letsencrypt"}
+        hosts:
+          - host: kmm.${var.domain}
+            paths:
+              - path: /
+                pathType: ImplementationSpecific
+        tls:
+          - secretName: kmm-tls
+            hosts:
+              - kmm.${var.domain}
+    EOT
+
+  ]
+
+  depends_on = [helm_release.kubeai]
+}
