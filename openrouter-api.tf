00942659347bc1ba8610a0c8e9a2cb6f3eb11b0f
Alin Florin <alinflorinciu@icloud.com>
2026-01-20 14:08:46 +0200

    asd

--- openrouter-api.tf
+++ openrouter-api.tf
@@ -1,14 +1,16 @@
 module "generic_app" {
   source         = "./modules/generic-node-app"
-  namespace = "generic"
-  name = "generic"
+  namespace = "bifrost"
+  name = "openrouter-api"
   location = var.location
   domain = var.domain
   ingress_enabled = true
-  enable_auth = true
+  ingress_path = "/openrouter/models"
+  ingress_subdomain = "bifrost"
+  enable_auth = false
   cpu_request = "50m"
   memory_request = "128Mi"
   cpu_limit = "100m"
   memory_limit = "256Mi"
-  depends_on = [ helm_release.longhorn[0] ]
+  depends_on = [ helm_release.kubeai ]
 }
