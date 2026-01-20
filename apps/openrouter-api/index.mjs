19c6ab59190f71e68c57cf5203f5a7e0ec20e985
Alin Florin <alinflorinciu@icloud.com>
2026-01-20 14:13:32 +0200

    asd

--- apps/openrouter-api/index.mjs
+++ apps/openrouter-api/index.mjs
@@ -1,7 +1,7 @@
 import http from 'http';
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/api/hello' && req.method === 'GET') {
+    if (req.url === '/openrouter/models' && req.method === 'GET') {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({ message: 'Hello World' }));
     } else {
