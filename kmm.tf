8f310aa241edb0887f3b13876b1cbba4322e2a6f
Alin Florin <alinflorinciu@icloud.com>
2026-01-20 08:46:58 +0200

    sdf

--- kmm.tf
+++ kmm.tf
@@ -4,7 +4,7 @@ resource "helm_release" "kmm" {
   chart            = "kmm"
   namespace        = "kubeai"
   create_namespace = false
-  version          = "1.0.9"
+  version          = "1.1.0"
   atomic           = true
   wait             = true
 
@@ -16,22 +16,42 @@ resource "helm_release" "kmm" {
         annotations:
           nginx.ingress.kubernetes.io/ssl-redirect: 'true'
           cert-manager.io/cluster-issuer: ${var.location == "local" ? "root-ca-issuer" : "letsencrypt"}
+          nginx.ingress.kubernetes.io/auth-url: "http://oauth2-proxy.oauth2-proxy.svc.cluster.local/oauth2/auth"
+          nginx.ingress.kubernetes.io/auth-signin: "https://oauth2-proxy.${var.domain}/oauth2/start?rd=$scheme://$host$request_uri"
+          nginx.ingress.kubernetes.io/proxy-buffering: "off"
+          nginx.ingress.kubernetes.io/proxy-read-timeout: "3600"
+          nginx.ingress.kubernetes.io/proxy-send-timeout: "3600"
         hosts:
           - host: kmm.${var.domain}
             paths:
               - path: /
-                pathType: ImplementationSpecific
+                pathType: Prefix
+        tls:
+          - secretName: kmm-tls
+            hosts:
+              - kmm.${var.domain}
+      publicIngress:
+        enabled: true
+        className: "nginx"
+        annotations:
+          nginx.ingress.kubernetes.io/ssl-redirect: 'true'
+          cert-manager.io/cluster-issuer: ${var.location == "local" ? "root-ca-issuer" : "letsencrypt"}
+        hosts:
+          - host: kmm.${var.domain}
+            paths:
+              - path: /public
+                pathType: Prefix
         tls:
           - secretName: kmm-tls
             hosts:
               - kmm.${var.domain}
       resources:
         requests:
-          cpu: "30m"
-          memory: "64Mi"
-        limits:
           cpu: "50m"
           memory: "128Mi"
+        limits:
+          cpu: "200m"
+          memory: "256Mi"
     EOT
 
   ]
