e631c638ad8cc894f5ecea0c503ce4fb1efcc7f7
Alin Florin <alinflorinciu@icloud.com>
2026-01-20 08:50:22 +0200

    ads

--- kmm.tf
+++ kmm.tf
@@ -4,7 +4,7 @@ resource "helm_release" "kmm" {
   chart            = "kmm"
   namespace        = "kubeai"
   create_namespace = false
-  version          = "1.1.0"
+  version          = "1.1.1"
   atomic           = true
   wait             = true
 
