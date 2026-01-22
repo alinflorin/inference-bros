112b2a4caf1b3515d8c08bf074dcd1ea8e1432b7
Alin Florin <alinflorinciu@icloud.com>
2026-01-22 12:17:10 +0200

    asd

--- bifrost.tf
+++ bifrost.tf
@@ -94,7 +94,7 @@ resource "helm_release" "bifrost" {
               trace_type: "otel"
               protocol: "http"
       image:
-        tag: 'v1.4.1'
+        tag: 'v1.4.2'
       replicaCount: ${var.bifrost_replicas}
       storage:
         mode: postgres
