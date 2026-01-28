864b1cf3260b6ac77cf6ef5a7101c8b2a638bb03
Alin Florin <alinflorinciu@icloud.com>
2026-01-28 11:15:13 +0200

    asd

--- control.tf
+++ control.tf
@@ -20,3 +20,55 @@ module "control" {
     "ODOO_TAX_ID"   = tostring(var.odoo_tax_id)
   }
 }
+
+
+resource "helm_release" "control_public_ingress" {
+  name             = "control-public-ingress"
+  repository       = "https://dasmeta.github.io/helm/"
+  chart            = "resource"
+  namespace        = "control"
+  create_namespace = true
+  version          = "0.1.0"
+  atomic           = true
+  wait             = true
+
+  values = [
+    <<-EOT
+      resource:
+        apiVersion: networking.k8s.io/v1
+        kind: Ingress
+        metadata:
+          name: control-public
+          namespace: control
+          labels:
+            app.kubernetes.io/instance: control
+            app.kubernetes.io/name: control
+          annotations:
+            cert-manager.io/cluster-issuer: ${var.location == "local" ? "root-ca-issuer" : "letsencrypt"}
+            nginx.ingress.kubernetes.io/ssl-redirect: 'true'
+        spec:
+          ingressClassName: nginx
+
+          tls:
+            - hosts:
+                - control.${var.domain}
+              secretName: control-tls
+
+          rules:
+            - host: control.${var.domain}
+              http:
+                paths:
+                  - path: /usage
+                    pathType: Prefix
+                    backend:
+                      service:
+                        name: control
+                        port:
+                          number: 80
+                  
+    EOT
+
+  ]
+
+  depends_on = [module.control]
+}
