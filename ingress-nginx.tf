fdc7fa0c38238be238d84ea6c4cf1eaf9706f415
Alin Florin <alinflorinciu@icloud.com>
2026-01-28 14:23:49 +0200

    asd

--- ingress-nginx.tf
+++ ingress-nginx.tf
@@ -33,6 +33,7 @@ resource "helm_release" "ingress_nginx" {
           otlp-collector-host: tempo.monitoring.svc.cluster.local
           otlp-collector-port: "4317"
           otel-service-name: ingress-nginx
+          opentelemetry-trust-incoming-span: "true"
           otel-sampler: AlwaysOn
         allowSnippetAnnotations: true
         service:
