f3a77c496c8ba92d3f1ef38b9872a9484ddf9863
Alin-Florin Ciu <alinflorinciu@icloud.com>
2026-01-29 09:31:46 +0200

    sdf

--- src/landing/src/main.tsx
+++ src/landing/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./routes/Home.tsx";
+import NotFound from "./routes/NotFound.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route element={<App />}>
+        <Route path="" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+);
