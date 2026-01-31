6c57493f97ec08feb5772b238a7423fa1625ef6f
Alin-Florin Ciu <alinflorinciu@icloud.com>
2026-01-31 23:10:11 +0200

    ZX

--- src/terraform/install_k3s.tf
+++ src/terraform/install_k3s.tf
@@ -105,7 +105,7 @@ resource "ssh_sensitive_resource" "install_k3s_first_master" {
     permissions = "0700"
   }
 
-  timeout = "20m"
+  timeout = "30m"
 
   pre_commands = [
     "mkdir -p /etc/rancher/k3s",
@@ -122,7 +122,7 @@ resource "ssh_sensitive_resource" "install_k3s_first_master" {
     "echo '${var.nginx_metallb_ip} dex.${var.domain}' | tee -a /etc/hosts",
     "curl -sL https://github.com/k3s-io/k3s/raw/main/contrib/util/generate-custom-ca-certs.sh | bash -",
     "curl -sfL https://get.k3s.io | sh -",
-    "sleep 30",
+    "sleep 180",
     "k3s kubectl create namespace cert-manager",
     "k3s kubectl create secret generic root-ca --from-file=tls.crt=/var/lib/rancher/k3s/server/tls/root-ca.pem --from-file=tls.key=/var/lib/rancher/k3s/server/tls/root-ca.key -n cert-manager",
     "helm repo add kube-vip https://kube-vip.github.io/helm-charts/",
