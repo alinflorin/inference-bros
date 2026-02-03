7d2fec6e0f291d04e1dddd55fe6f566ed4851c44
Alin Florin <alinflorinciu@icloud.com>
2026-02-04 00:51:20 +0200

    asd

--- src/terraform/backup.tf
+++ src/terraform/backup.tf
@@ -28,23 +28,27 @@ resource "helm_release" "snapshot_controller" {
 
 resource "helm_release" "longhorn_volume_snapshot_class" {
   name             = "longhorn-volume-snapshot-class"
-  repository       = "https://ameijer.github.io/k8s-as-helm"
-  chart            = "volumesnapshotclass"
+  repository       = "https://charts.appuio.ch"
+  chart            = "generic"
   namespace        = "longhorn-system"
   create_namespace = true
-  version          = "1.0.0"
+  version          = "0.1.2"
   atomic           = true
   wait             = true
 
   values = [
     <<-EOT
-      apiVersion: snapshot.storage.k8s.io/v1
-      annotations: {}
-      labels:
-        velero.io/csi-volumesnapshot-class: "true"
-      driver: driver.longhorn.io
-      parameters:
-        type: bak
+      resources: |
+        kind: VolumeSnapshotClass
+        apiVersion: snapshot.storage.k8s.io/v1
+        metadata:
+          name: longhorn-snapshot-vsc
+          labels:
+            velero.io/csi-volumesnapshot-class: "true"
+        driver: driver.longhorn.io
+        deletionPolicy: Delete
+        parameters:
+          type: bak
     EOT
 
   ]
