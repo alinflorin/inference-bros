03bd5568c60941a7423ff92819ddfe1bb0a33208
Alin Florin <6534913+alinflorin@users.noreply.github.com>
2026-02-08 11:23:31 +0200

    Website look update (#43)
    * look update
    
    * rm
    
    * green leaf
    
    ---------
    
    Co-authored-by: Crunch Machine: Genesis <sorin@qoffice.ro>

--- src/landing/src/main.tsx
+++ src/landing/src/main.tsx
@@ -1,28 +1,5 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router";
-import Home from "./routes/Home.tsx";
-import NotFound from "./routes/NotFound.tsx";
-import { FluentProvider } from "@fluentui/react-components";
-import { customTheme } from "./theme.ts";
-import About from "./routes/About.tsx";
-import Contact from "./routes/Contact.tsx";
-import Services from "./routes/Services.tsx";
+import "./index.css";
 
-
-createRoot(document.getElementById("root")!).render(
-  <FluentProvider theme={customTheme} style={{height: '100vh'}}>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<App />}>
-          <Route path="" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="services" element={<Services />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </FluentProvider>,
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
