fbe3087a957b18b0bc008cc5f294f9272037eb12
Alin Florin <alinflorinciu@icloud.com>
2026-02-19 19:00:51 +0200

    -

--- src/terraform/tempo.tf
+++ src/terraform/tempo.tf
@@ -4,7 +4,7 @@ resource "helm_release" "tempo" {
   chart            = "tempo"
   namespace        = "monitoring"
   create_namespace = true
-  version          = "1.26.4"
+  version          = "1.26.3"
   atomic           = true
   wait             = true
 
